f70e0e1a0569b3f1d2e92d32376d20f3fde071d9
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-29 17:53:13 +0500

    Update 29-05-2023

--- CalculatorUsingEnum/Program.cs
+++ CalculatorUsingEnum/Program.cs
@@ -4,32 +4,38 @@ using CalculatorUsingEnum;
 int userInput1, userInput2, userSelector, result = 0;
 //Code
 Console.Write("Enter First Value: ");
-userInput1 = Convert.ToInt32(Console.ReadLine());
+userInput1 = Convert.ToInt32(Console.ReadLine());   //Input Value one
 Console.Write("Enter Second Value: ");
-userInput2 = Convert.ToInt32(Console.ReadLine());
+userInput2 = Convert.ToInt32(Console.ReadLine());   //Input Value Two
+//Calling Values From Enum
 Console.WriteLine($"{(int)Actions.Add} - {Actions.Add}\n{(int)Actions.Subtract} - {Actions.Subtract}\n{(int)Actions.Multiply} - {Actions.Multiply}\n{(int)Actions.Divide} - {Actions.Divide}\n{(int)Actions.Modulo} - {Actions.Modulo}");
 Console.Write(">>");
-userSelector = Convert.ToInt32(Console.ReadLine());
+userSelector = Convert.ToInt32(Console.ReadLine()); //User Selector
+//Cases According to User
 switch (userSelector)
 {
     case (int)Actions.Add:
         result = userInput1 + userInput2;
+        Console.WriteLine($"Your Answer is {result}");
         break;
     case (int)Actions.Subtract:
-        result = userInput1 + userInput2;
+        result = userInput1 - userInput2;
+        Console.WriteLine($"Your Answer is {result}");
         break;
     case (int)Actions.Multiply:
-        result = userInput1 + userInput2;
+        result = userInput1 * userInput2;
+        Console.WriteLine($"Your Answer is {result}");
         break;
     case (int)Actions.Divide:
-        result = userInput1 + userInput2;
+        result = userInput1 / userInput2;
+        Console.WriteLine($"Your Answer is {result}");
         break;
     case (int)Actions.Modulo:
-        result = userInput1 + userInput2;
+        result = userInput1 % userInput2;
+        Console.WriteLine($"Your Answer is {result}");
         break;
     default:
         Console.WriteLine("Invalid Input");
         break;
 }
-Console.WriteLine($"Your Answer is {result}");
 Console.ReadLine();
