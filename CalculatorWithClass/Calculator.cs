510a32148ccc3eea02820ccebd6659fa3f483ef6
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-06-14 12:29:52 +0500

    Update 14-06-2023

---
+++ CalculatorWithClass/Calculator.cs
@@ -0,0 +1,33 @@
+﻿namespace CalculatorWithClass
+{
+    internal class Calculator
+    {
+        private double value1;
+        private double value2;
+        public void SetValue(double value1, double value2)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+        public void Add()
+        {
+            Console.WriteLine($"{value1} + {value2} = {value1+value2}");
+        }
+        public void Subtract()
+        {
+            Console.WriteLine($"{value1} - {value2} = {value1 - value2}");
+        }
+        public void Multiply()
+        {
+            Console.WriteLine($"{value1} x {value2} = {value1 * value2}");
+        }
+        public void Divid()
+        {
+            Console.WriteLine($"{value1} / {value2} = {value1 / value2}");
+        }
+        public void Modules()
+        {
+            Console.WriteLine($"{value1} % {value2} = {value1 % value2}");
+        }
+    }
+}
---
+++ CalculatorWithClass/Program.cs
@@ -0,0 +1,37 @@
+﻿//Linked Files
+using CalculatorWithClass;
+//Objects
+Calculator cal = new Calculator();
+//Variables
+double value1, value2;
+int selector;
+//Code
+Console.Write("Enter First Value: ");
+value1 = int.Parse(Console.ReadLine());
+Console.Write("Enter Second Value: ");
+value2 = int.Parse(Console.ReadLine());
+cal.SetValue(value1, value2);
+Console.Write("----Select Option----\n1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division\n5 - Modulus\n>> ");
+selector = int.Parse(Console.ReadLine());
+switch (selector)
+{
+    case 1:
+        cal.Add();
+        break;
+    case 2:
+        cal.Subtract();
+        break;
+    case 3:
+        cal.Multiply();
+        break;
+    case 4:
+        cal.Divid();
+        break;
+    case 5:
+        cal.Modules();
+        break;
+    default:
+        Console.WriteLine("Invalid Input");
+        break;
+}
+Console.ReadLine();
