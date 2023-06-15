f3cd6c0ba6ce9b6579ae77e7d01ddf02b3fc6cca
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-06-15 13:03:57 +0500

    Update 15-06-2023

---
+++ StudentInfoGetherWithMarks/Program.cs
@@ -0,0 +1,24 @@
+﻿//Link Files
+using StudentInfoGetherWithMarks;
+//Object
+Student std = new Student();
+//Variables
+string stdName;
+int stdClass,stdAge;
+int[] stdMarks = new int[5];
+//Code
+Console.Write("Enter Student Name: ");
+stdName = Console.ReadLine();
+Console.Write("Enter Student Class: ");
+stdClass = int.Parse(Console.ReadLine());
+Console.Write("Enter Student Age: ");
+stdAge = int.Parse(Console.ReadLine());
+for (int i = 0; i <stdMarks.Length; i++)
+{
+    Console.Write($"Enter Marks For {i+1} Subject: ");
+    stdMarks[i] = int.Parse(Console.ReadLine());
+}
+std.SetInfo(stdName,stdClass,stdAge,stdMarks);
+Console.Clear();
+std.GetInfo();
+Console.ReadLine();
---
+++ StudentInfoGetherWithMarks/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentInfoGetherWithMarks
+{
+    internal class Student
+    {
+        string stdName;
+        int stdAge, stdClass, totalMarks = 0;
+        int[] stdMarks;
+        public void SetInfo(string stdName, int stdClass, int stdAge, int[] stdMarks)
+        {
+            this.stdName = stdName;
+            this.stdAge = stdAge;
+            this.stdMarks = stdMarks;
+            Array.Copy(stdMarks, 0, this.stdMarks, 0, stdMarks.Length);
+        }
+        public void GetInfo()
+        {
+            int a = 1;
+            Console.WriteLine($"Student Name: {stdName}\nStudent Age: {stdAge}\nStudent Class: {stdClass}\n--------------");
+            foreach (int item in stdMarks)
+            {
+                totalMarks += item;
+                Console.WriteLine($"Subject {a} Marks : {item}");
+                a++;
+            }
+            Console.WriteLine($"Subjects Total Marks : {totalMarks}");
+        }
+    }
+}
